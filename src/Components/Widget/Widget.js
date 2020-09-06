7a8e55f0819940832ad1d65fbc9e320dd610e2d0
hogni-seoul <hogni@kakao.com>
2020-09-06 22:52:46 +0900

    위젯 페이지 변경

--- src/Components/Widget/Widget.js
+++ src/Components/Widget/Widget.js
@@ -11,7 +11,7 @@ function Widget() {
     <Box display={{ xs: "none", md: "block" }}>
       <Iframe
         title="facebook-widget"
-        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Freact%2F&tabs=timeline&width=360&height=1500&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=false&appId"
+        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FMTV&tabs=timeline&width=360&height=1500&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=false&appId"
         scrolling="no"
         frameBorder="0"
         allow="encrypted-media"
